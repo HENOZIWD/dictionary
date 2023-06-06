5a73d76f41bf6183e24f7a59dccdacde7f12f008
Donghyeon Lee <ehdgus001928@gmail.com>
2023-06-07 04:52:29 +0900

    fix: Fix dynamic server error
    DynamicServerError: Dynamic server usage is occured during build.
    Therefore make api fully dynamic.

--- src/app/dictionary/getWords/api/route.ts
+++ src/app/dictionary/getWords/api/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '../../lib/client';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const newEntry = await prisma.dictionary.findMany();
