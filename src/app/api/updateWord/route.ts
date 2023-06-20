d7264bfb9a1d51b400a04b566dc7f4e1081b5644
Donghyeon Lee <ehdgus001928@gmail.com>
2023-06-20 16:49:12 +0900

    fix: Fix module import error

--- src/app/api/updateWord/route.ts
+++ src/app/api/updateWord/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '../../../lib/client';
+import { prisma } from '@/app/lib/client';
 
 export const dynamic = 'force-dynamic';
 
